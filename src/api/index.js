936cb2d8e4590290f7ce27dbf99a8db4fdc85536
Sebastián <sebaavitiello@gmail.com>
2024-09-27 02:12:47 -0300

    ADD: api functions

---
+++ src/api/index.js
@@ -0,0 +1,49 @@
+import axios from 'axios';
+import { uppercase } from '../utils';
+
+const BASE_URL = 'https://dog.ceo/api';
+
+export const getRandomImage = async () => {
+  const { data } = await axios.get(`${BASE_URL}/breeds/image/random`);
+  const { message: randomImageUrl, status } = data || {};
+  if (status !== 'success') return '';
+  return randomImageUrl;
+};
+
+export const getRandomByBreed = async finalBreed => {
+  const { data } = await axios.get(
+    `${BASE_URL}/breed/${finalBreed}/images/random`
+  );
+  const { message: randomImageUrl, status } = data || {};
+  if (status !== 'success') return '';
+  return randomImageUrl;
+};
+
+export const getAllBreeds = async () => {
+  const { data } = await axios.get(`${BASE_URL}/breeds/list/all`);
+  const { message: breeds, status } = data || {};
+  if (status !== 'success') return [];
+
+  const parsedBreeds = Object.entries(breeds).map(breed => {
+    const hasSubBreeds = breed[1]?.length;
+    return {
+      name: breed[0],
+      label: uppercase(breed[0]),
+      ...(hasSubBreeds && {
+        subBreeds: breed[1].map(subBreed => ({
+          name: subBreed,
+          label: uppercase(subBreed),
+        })),
+      }),
+    };
+  });
+  return parsedBreeds;
+};
+
+export const getSubBreedsByBreed = async breed => {
+  if (!breed) return [];
+  const { data } = await axios.get(`${BASE_URL}/breed/${breed}/list`);
+  const { message: subBreeds, status } = data || {};
+  if (status !== 'success') return [];
+  return subBreeds;
+};
