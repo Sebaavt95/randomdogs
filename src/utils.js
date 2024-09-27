1f19ea331db1360561aa253bc3f1de99bb91f6f9
Sebastián <sebaavitiello@gmail.com>
2024-09-27 02:13:39 -0300

    CHANGE: helpers filename (replaced by utils), functions inside it

---
+++ src/utils.js
@@ -0,0 +1,14 @@
+export const uppercase = str =>
+  str ? str.charAt(0).toUpperCase() + str.slice(1) : '';
+
+export const getBreedName = url => {
+  if (!url) return;
+  const name = url.slice(30, url.indexOf('/', 30));
+  const breedName = name.includes('-') ? name.replace('-', ' ') : name;
+  return uppercase(breedName);
+};
+
+export const removeNumbers = id => {
+  if (!id) return;
+  return id.replace(/[0-9]/gi, '');
+};
